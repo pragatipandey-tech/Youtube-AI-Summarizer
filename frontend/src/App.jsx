9cf3e4684225b719f0157a24c6f21519ef83b715
pragatipandey-tech <pragatipandey.ds@gmail.com>
2026-05-21 23:31:13 +0530

    Create reusable URL input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,26 @@
 import Navbar from "./components/Navbar"
+import UrlInput from "./components/UrlInput"
+
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-4">
+    <>
+      <Navbar />
 
-      <h1 className="text-5xl font-bold mb-4 text-center">
-        AI YouTube Summarizer
-      </h1>
+      <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-4">
 
-      <p className="text-gray-400 mb-8 text-center max-w-xl">
-        Paste a YouTube link and get AI-generated summaries,
-        notes, timestamps and quiz questions instantly.
-      </p>
+        <h1 className="text-5xl font-bold mb-4 text-center">
+          AI YouTube Summarizer
+        </h1>
 
-      <div className="w-full max-w-2xl flex gap-3">
-        <input
-          type="text"
-          placeholder="Paste YouTube URL..."
-          className="flex-1 p-4 rounded-xl bg-zinc-900 border border-zinc-700 outline-none"
-        />
+        <p className="text-gray-400 mb-8 text-center max-w-xl">
+          Paste a YouTube link and get AI-generated summaries,
+          notes, timestamps and quiz questions instantly.
+        </p>
 
-        <button className="bg-red-600 hover:bg-red-700 px-6 rounded-xl font-semibold">
-          Summarize
-        </button>
+        <UrlInput />
+
       </div>
-
-    </div>
+    </>
   )
 }
 
--- frontend/src/components/UrlInput.jsx
+++ frontend/src/components/UrlInput.jsx
@@ -1,6 +1,18 @@
 function UrlInput() {
   return (
-    <div>UrlInput</div>
+    <div className="w-full max-w-2xl flex gap-3">
+
+      <input
+        type="text"
+        placeholder="Paste YouTube URL..."
+        className="flex-1 p-4 rounded-xl bg-zinc-900 border border-zinc-700 outline-none"
+      />
+
+      <button className="bg-red-600 hover:bg-red-700 px-6 rounded-xl font-semibold">
+        Summarize
+      </button>
+
+    </div>
   )
 }
 
