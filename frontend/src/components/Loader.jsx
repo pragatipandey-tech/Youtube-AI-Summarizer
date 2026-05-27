4c2f17cf8f5b7ca472dac2d868e1f4cece93669d
pragatipandey-tech <pragatipandey.ds@gmail.com>
2026-05-27 23:22:28 +0530

    Create animated loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,6 +1,10 @@
 function Loader() {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center mt-8">
+
+      <div className="w-12 h-12 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></div>
+
+    </div>
   )
 }
 
