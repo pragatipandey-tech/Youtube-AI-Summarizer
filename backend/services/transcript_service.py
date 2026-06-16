ef81d4f084dd8f34566e7a8ae678d08637471323
pragatipandey-tech <pragatipandey.ds@gmail.com>
2026-06-16 14:40:29 +0530

    Add exception handling to transcript service

--- backend/services/transcript_service.py
+++ backend/services/transcript_service.py
@@ -2,16 +2,21 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 def fetch_transcript(video_id):
 
-    api = YouTubeTranscriptApi()
+    try:
 
-    transcript_list = api.fetch(video_id)
+        api = YouTubeTranscriptApi()
 
-    transcript_text = " ".join(
-        [item.text for item in transcript_list]
-    )
+        transcript_list = api.fetch(video_id)
 
-    transcript_text = transcript_text.replace("\n", " ")
-    transcript_text = transcript_text.replace("♪", "")
-    transcript_text = transcript_text.strip()
+        transcript_text = " ".join(
+            [item.text for item in transcript_list]
+        )
 
-    return transcript_text
+        transcript_text = transcript_text.replace("\n", " ")
+        transcript_text = transcript_text.replace("♪", "")
+        transcript_text = transcript_text.strip()
+
+        return transcript_text
+
+    except Exception:
+        return None
